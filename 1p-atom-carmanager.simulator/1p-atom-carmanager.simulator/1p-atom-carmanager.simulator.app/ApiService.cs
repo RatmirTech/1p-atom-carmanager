0bccfcc3f2cfc2368371113c3122b8ac530e01eb
Ratmri Ishgulov <ishgulov.ratmir@gmail.com>
2023-09-19 01:03:26 +0300

    #2 Init project simulator

---
+++ 1p-atom-carmanager.simulator/1p-atom-carmanager.simulator/1p-atom-carmanager.simulator.app/ApiService.cs
@@ -0,0 +1,45 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace _1p_atom_carmanager.simulator.app;
+
+public class ApiService
+{
+    private static readonly HttpClient client = new HttpClient();
+    private static readonly string baseUrl = "http://atomkai1p-001-site1.dtempurl.com/";
+
+    public static async Task<string> Get(string endpoint)
+    {
+        string result = null;
+        HttpResponseMessage response = await client.GetAsync(baseUrl + endpoint);
+        if (response.IsSuccessStatusCode)
+        {
+            result = await response.Content.ReadAsStringAsync();
+        }
+        return result;
+    }
+
+    public static async Task<string> Post(string endpoint, string payload)
+    {
+        string result = null;
+        HttpResponseMessage response = await client.PostAsync(baseUrl + endpoint, new StringContent(payload));
+        if (response.IsSuccessStatusCode)
+        {
+            result = await response.Content.ReadAsStringAsync();
+        }
+        return result;
+    }
+
+    public static async Task<bool> Delete(string endpoint)
+    {
+        HttpResponseMessage response = await client.DeleteAsync(baseUrl + endpoint);
+        return response.IsSuccessStatusCode;
+    }
+
+    public static async Task<bool> Put(string endpoint, string payload)
+    {
+        HttpResponseMessage response = await client.PutAsync(baseUrl + endpoint, new StringContent(payload));
+        return response.IsSuccessStatusCode;
+    }
+}
+
---
+++ 1p-atom-carmanager.simulator/1p-atom-carmanager.simulator/1p-atom-carmanager.simulator.app/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+
+namespace _1p_atom_carmanager.simulator.app
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void SubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string result = await ApiService.Get("info/get/");
+                ResultLabel.Content = "Result: " + result;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
